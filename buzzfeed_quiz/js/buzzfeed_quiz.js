b8353be92873a943b6cb5d604954ddf5c89e2119
Olivia Liebman <olivialiebman@yahoo.com>
2016-12-19 19:02:05 +0200

    version 2

--- buzzfeed_quiz/js/buzzfeed_quiz.js
+++ buzzfeed_quiz/js/buzzfeed_quiz.js
@@ -9,16 +9,21 @@ function checkChecked(stayChecked) {
        	}
     }
 }                    
-function Q1clicked() {
+function Qclicked() {
 var input_val = document.getElementsByClassName('Q1'); //input_val is now an array of the values from Question 1
-	for (var i=0; input_val.length; ++i) {
+var input_valQ3 = document.getElementsByClassName('Q3'); 
+	for (var i=0; i < input_val.length; ++i) {
 		if(input_val[i].checked) {
-			// counter = input_val[i].value;
 			var val_int = parseInt(input_val[i].value);
-			counter =+ val_int;
+			counter += val_int;
 			console.log(counter);
-			break;
 		}
+		 	if (input_valQ3.item(i).checked==true) {
+			var val_int1 = parseInt(input_valQ3[i].value);
+			counter += val_int1;
+		}
+		else {continue;}
+
 	}
 }
 function Q2clicked() {
@@ -27,50 +32,34 @@ var val_int = parseInt(input_val);
 	for (var i=0; i < 4; ++i) {
 		if(i == val_int) {
 			counter += i;
-			console.log(counter);
 			break;
 		}
-	}
-}
-function Q3clicked() {
-var input_val = document.getElementsByClassName('Q3'); 
-	for (var i=0; i < input_val.length; ++i) {
- 		if (input_val.item(i).checked===true) {
-			var val_int = parseInt(input_val[i].value);
-			counter += val_int;
-			console.log(counter);
-			break;
-		}
-		else {continue;}
 	}
 }
 function Q4clicked(clicked_id) {
 	var value = parseInt(clicked_id);
 	counter += value;
-	console.log(counter);
+
 }
-function Q5clicked() {
+function finalSubmit() {
+	Q2clicked();
+	Qclicked(); 
+
 	var input_val = document.getElementById('answer5').value; 
 	var val_int = parseInt(input_val);	
 	counter += val_int;
-	console.log(counter);
-}
-function finalSubmit() {
-	if (counter <=5) {
-		document.getElementById("finalOutcome").innerHTML="You are completely sane";
 
-		// document.write("You are completely sane");
+	if (counter <=5) {
+		alert("You are completely sane");
 	}
 	else if(counter > 5 && counter <=10) {
-		document.getElementById("finalOutcome").innerHTML="You got some quirks, but nothin' to stress about";
+		alert("You got some quirks, but nothin' to stress about");
 	}
 	else if(counter > 10 && counter <=15) {
-		document.getElementById("finalOutcome").innerHTML="May want to see somebody...";
+		alert("May want to see somebody...");
 	}
 	else {
-		document.getElementById("finalOutcome").innerHTML="Please put on this nice, fluffy, white jacket, and come with me...";
+		alert("Please put on this nice, fluffy, white jacket, and come with me...");
 	}
 }
-
-
 
